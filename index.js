d9285fda6c004c653d786c1055015f2bc67621ff
hoyipyik <hoyipyik@protonmail.com>
2021-11-30 10:56:20 +0800

    full with legacy

--- index.js
+++ index.js
@@ -34,33 +34,3 @@ function handleStateChange(){
     }
 }
 
-var flag = true
-
-function showSubnav(){
-    // var flag = true
-    // console.log(flag)
-    var subnav = '<ul id="sub-nav-ul">\
-    <li>\
-        <a class="nonhome" target="_blank" href="https://en.wikipedia.org/wiki/White_Album_2">Wikipedia</a>\
-    </li>\
-    <li>\
-        <a class="nonhome" target="_blank" href="https://www.imdb.com/title/tt2942224/">IMDb</a>\
-    </li>\
-    <li>\
-        <a class="nonhome" href="./donate.html">Donate</a>\
-    </li>\
-    </ul>';
-    var br = "<br><br><br>"
-    if(flag){
-        document.getElementById('more').innerHTML = "Hide"
-        document.getElementById("sub-nav").innerHTML = subnav
-        document.getElementById("br").innerHTML = br
-        flag = !flag
-    }else{
-        document.getElementById('more').innerHTML = "More"
-        document.getElementById("sub-nav").innerHTML = null
-        document.getElementById("br").innerHTML = null
-        flag = !flag
-    }
-    
-}       
